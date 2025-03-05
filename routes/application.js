51e91f8c032451ec534f0a6d3c367a20c33c3a96
rutujadautpure <rutujadautpure05@gmail.com>
2025-03-06 03:18:00 +0530

    to see all appln

--- routes/application.js
+++ routes/application.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router();
 const {handleApplyForm, getAllJobs} = require("../controllers/application");
-
+//const { isLoggedIn } = require("../middleware");
 router.get('/home/:id',async(req,res)=>{
     const id=req.params.id;
     return res.render("./worker/home", {id:id});   
@@ -11,7 +11,7 @@ router.get('/applyForm/:id/:hiringId',async(req,res)=>{
     const hiringid=req.params.hiringId;
     return res.render("./worker/applicationForm", {id:id, hiringid:hiringid});   
 });
-router.post('/application/:id/:hiringId',handleApplyForm);
+router.post('/application/:id/:hiringId',handleApplyForm);  
 
 router.get("/alljobs/:id", getAllJobs);
 
